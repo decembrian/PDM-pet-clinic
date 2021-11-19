0844e28196a8ca3e1d681f74545e1883f704c157
Pavel Zakharov <zaharovpo4@gmail.com>
2021-11-20 00:01:33 +0300

    initial project commit

---
+++ pet-clinic-data/src/main/java/pdm/petclinic/poshadcmpetclinic/PoshaDcmPetClinicApplication.java
@@ -0,0 +1,13 @@
+package pdm.petclinic.poshadcmpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoshaDcmPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoshaDcmPetClinicApplication.class, args);
+    }
+
+}
---
+++ pet-clinic-web/src/test/java/pdm/petclinic/poshadcmpetclinic/PoshaDcmPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package pdm.petclinic.poshadcmpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoshaDcmPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
