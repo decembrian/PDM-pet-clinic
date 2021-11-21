7a2a4c89a78e415f8d762b6e63463401b78b832a
Pavel Zakharov <zaharovpo4@gmail.com>
2021-11-21 17:18:46 +0300

    refactoring to common crud interface. closes #8

--- pet-clinic-data/src/main/java/pdm/petclinic/poshadcmpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/pdm/petclinic/poshadcmpetclinic/services/PetService.java
@@ -2,11 +2,7 @@ package pdm.petclinic.poshadcmpetclinic.services;
 
 import pdm.petclinic.poshadcmpetclinic.model.Pet;
 
-import java.util.Set;
 
-public interface PetService {
+public interface PetService extends CrudService<Pet, Long>{
 
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
 }
