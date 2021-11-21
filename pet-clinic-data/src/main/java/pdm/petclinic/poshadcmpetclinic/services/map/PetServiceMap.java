2d28440bfe687c40705620004be1a44367aeeb98
Pavel Zakharov <zaharovpo4@gmail.com>
2021-11-21 18:00:41 +0300

    implement map based services. closes #2

---
+++ pet-clinic-data/src/main/java/pdm/petclinic/poshadcmpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package pdm.petclinic.poshadcmpetclinic.services.map;
+
+import pdm.petclinic.poshadcmpetclinic.model.Pet;
+import pdm.petclinic.poshadcmpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.map.put(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/pdm/petclinic/poshadcmpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package pdm.petclinic.poshadcmpetclinic.services.map;
+
+import pdm.petclinic.poshadcmpetclinic.model.Vet;
+import pdm.petclinic.poshadcmpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.map.put(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
