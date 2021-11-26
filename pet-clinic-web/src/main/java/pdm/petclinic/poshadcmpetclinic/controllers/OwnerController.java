f1242887f697198df9e2cabac93f848693227b04
Pavel Zakharov <zaharovpo4@gmail.com>
2021-11-26 22:22:17 +0300

    implement spring config for services. closes #19

--- pet-clinic-web/src/main/java/pdm/petclinic/poshadcmpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/pdm/petclinic/poshadcmpetclinic/controllers/OwnerController.java
@@ -2,14 +2,24 @@ package pdm.petclinic.poshadcmpetclinic.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pdm.petclinic.poshadcmpetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+   private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOfOwners(){
+    public String listOfOwners(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index";
     }
