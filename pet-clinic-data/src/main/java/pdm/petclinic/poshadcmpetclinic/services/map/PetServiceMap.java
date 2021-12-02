4a6446f05458bd7456cbf15f46b4598d1c26cc56
decembrian <zaharovpo4@gmail.com>
2021-12-02 22:11:04 +0300

    refactor map impl to abstract id generation. closes #20

--- pet-clinic-data/src/main/java/pdm/petclinic/poshadcmpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/pdm/petclinic/poshadcmpetclinic/services/map/PetServiceMap.java
@@ -21,7 +21,7 @@ public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetS
 
     @Override
     public Pet save(Pet object) {
-        return super.map.put(object.getId(), object);
+        return super.save(object);
     }
 
     @Override
